24e00a3203e790df56b96ea61b4bc6e294c3e281
王成飞 <764297968@qq.com>
2017-10-27 16:35:54 +0800

    update

--- js/cfg.js
+++ js/cfg.js
@@ -1,5 +1,52 @@
 var cfg = {
-	keyname: "bill"
+	keyname: "bill",
+	downloadkey: "DownLoadPath",
+	downloadpath: function() {
+		var path = plus.storage.getItem(cfg.downloadkey);
+
+		if(path == null) {  
+			plus.io.resolveLocalFileSystemURL("_downloads/", function(entry) {
+					path = entry.fullPath.substr(0, entry.fullPath.indexOf('Android')) + "Download/";
+					plus.storage.setItem(cfg.downloadkey, path);
+					return path;
+				},
+				function(err) {
+					console.log(err.message);
+				})
+
+		} else {
+			return path;
+		}
+
+	},
+	dateToStr: function(datetime) {
+		var year = datetime.getFullYear(),
+			month = datetime.getMonth() + 1,
+			date = datetime.getDate(),
+			hour = datetime.getHours(),
+			minutes = datetime.getMinutes(),
+			second = datetime.getSeconds();
+		if(month < 10) {
+			month = "0" + month;
+		}
+		if(date < 10) {
+			date = "0" + date;
+		}
+		if(hour < 10) {
+			hour = "0" + hour;
+		}
+		if(minutes < 10) {
+			minutes = "0" + minutes;
+		}
+		if(second < 10) {
+			second = "0" + second;
+		}
+		return(year + "-" + month + "-" + date + " " + hour + ":" + minutes + ":" + second);
+	},
+	setstorge:function(key,value)
+	{
+		plus.storage.setItem(key,value);
+	}
 };
 
 var db = window.openDatabase("mydatabase", "1.0", "我的数据库描述", 20000);
@@ -16,48 +63,34 @@ var sql = "CREATE TABLE bill (" +
 db.transaction(function(tx) {
 	tx.executeSql(sql);
 });
-//
-//
-//db.transaction(function(tx) {
-//	//tx.executeSql("insert into mytable (mytitle,timestamp) values('111',"+d.toLocaleString()+")",[],null,null);
-//	try{
-//		tx.executeSql("insert into bill (id,money,des,date,createtime,state) values(?,?,?,?,?,?)", [d.getTime(), '22', '吃的', d.toLocaleString(),d.toLocaleString(), 1], null, null);
-//	}catch(e){
-//		//TODO handle the exception
-//		console.log(e);
-//	}
-//});
-db.query = function( sql, func){
-	if(db && sql){
-		db.transaction(function(tx){
+db.query = function(sql, func) {
+	if(db && sql) {
+		db.transaction(function(tx) {
 			tx.executeSql(sql, [], function(tx, results) {
 				func(results);
 			}, null);
 		});
 	}
 };
-db.addbill=function(m,des,t)
-{
+db.addbill = function(m, des, t) {
 	db.transaction(function(tx) {
-	tx.executeSql("insert into bill (id,money,des,date,createtime,state) values(?,?,?,?,?,?)", [d.getTime(), m, des,t,d.toLocaleString(), 1], null, null);
+		tx.executeSql("insert into bill (id,money,des,date,createtime,state) values(?,?,?,?,?,?)", [d.getTime(), m, des, t, d.toLocaleString(), 1], null, null);
 	})
-	
+
 }
-db.showbill=function(func)
-{
-	
-	db.query("select * from bill order by createtime desc",function(res){
-		var data=[];
-		for (var i=0;i<res.rows.length;i++) {
+db.showbill = function(func) {
+
+	db.query("select * from bill order by createtime desc", function(res) {
+		var data = [];
+		for(var i = 0; i < res.rows.length; i++) {
 			data.push(res.rows.item(i));
 		}
 		func(data);
 	})
 }
-db.delbill=function()
-{
+db.delbill = function() {
 	db.transaction(function(tx) {
-	tx.executeSql("delete from bill", [], null, null);
+		tx.executeSql("delete from bill", [], null, null);
 	})
 }
 //db.addbill(23,"呵呵呵",d.toLocaleString())
